6dcb3e6994b87a9d8da1c958f9c3de90f12f5963
atoor0807 <atoor0807@gmail.com>
2025-12-13 17:50:33 +0500

    persist on page reloads

--- day4/script.js
+++ day4/script.js
@@ -1,6 +1,16 @@
 let themetoggle = document.getElementById('themetoggle')
 
+document.addEventListener('DOMContentLoaded',()=>{
+    let theme = localStorage.getItem("theme")
+    if(theme === "dark"){
+        document.body.classList.add('dark')
+    }
+
+})
+
 themetoggle.addEventListener('change',()=>{
     //change looks for any change in the input value. 
     document.body.classList.toggle('dark')
+    let p = document.body.classList.value
+    localStorage.setItem("theme",p)
 })
